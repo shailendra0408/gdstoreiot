07c8aa1e7fd8a00cda28cf6b35b6c05a187a7acf
Shailendra <srj0408@gmail.com>
2019-06-05 07:02:36 +0530

    stored data in the list and created seperate functions to do so

--- gdtestfolder/gdstore_test.py
+++ gdtestfolder/gdstore_test.py
@@ -1,14 +1,36 @@
 from google.cloud import datastore
+
+#global variables declaration 
+templist = []
+humlist = []
+
+
 client = datastore.Client()
 query = client.query(kind='senval')
 query.add_filter('Temp', '>', 2.00)
 #query.add_filter('Humidity', '>', 10.00)
 result = list(query.fetch())
-print result
+#print result
 
-for data in result:
-    try:
-        print "Temperature : {0}".format(data['Temp'])
-        print "Humidity : {0}".format(data['Humidity '])
-    except KeyError:
-       pass 
+
+def prepare_templist(result):
+    for data in result:
+        try:
+            print "Temperature : {0}".format(data['Temp'])
+            templist.append(data['Temp'])
+            print templist 
+        except KeyError:
+           pass 
+
+def prepare_humiditylist(result):
+    for data in result:
+        try:
+            print "Humidity : {0}".format(data['Humidity '])
+            humlist.append(data['Humidity '])
+            print humlist
+        except KeyError:
+           pass
+
+if __name__ == "__main__":
+    prepare_templist(result)
+    prepare_humiditylist(result)
