9ec286aefb4e6568bedd9fc1e8287df52fe3f189
Shailendra <srj0408@gmail.com>
2019-08-18 23:54:55 +0530

    used the data from rootsprint - its a string, a lot of data massage would be required. Right now able to fetch the data from gdstore

--- gdtestfolder/gdstore_test.py
+++ gdtestfolder/gdstore_test.py
@@ -1,17 +1,29 @@
 from google.cloud import datastore
-
+import re
 #global variables declaration 
 templist = []
 humlist = []
-
+datalist = []
+parseddata = []
 
 client = datastore.Client()
-query = client.query(kind='senval')
-query.add_filter('Temp', '>', 2.00)
+query = client.query(kind='Thermo')
+query.add_filter('device_id', '=', '1c002e000347363339343638')
 #query.add_filter('Humidity', '>', 10.00)
-result = list(query.fetch())
+result = list(query.fetch(limit = 10))
 #print result
 
+def prepare_data(result):
+    for data in result: 
+        try:
+            print "data is : {0} and published time is {1}".format(data['data'], data['published_at'])
+            datalist.append(data['data'])
+            parseddata = re.split(',', data['data'])
+            for i in parseddata:
+                print i
+            #print datalist 
+        except KeyError:
+            pass
 
 def prepare_templist(result):
     for data in result:
@@ -32,5 +44,6 @@ def prepare_humiditylist(result):
            pass
 
 if __name__ == "__main__":
-    prepare_templist(result)
-    prepare_humiditylist(result)
+    prepare_data(result)
+    #prepare_templist(result)
+    #prepare_humiditylist(result)
