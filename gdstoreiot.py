a79a34697d668d4d9592eb2459dfd5108a9636de
Shailendra <srj0408@gmail.com>
2019-08-25 01:07:12 +0530

    Working code which return temp + timestamp dict
    1. Now add other variables.
    2. Ideally last 10,100,1000 queries request should come from the
    frontend.
    3. By default it should be 10 only

--- gdstoreiot.py
+++ gdstoreiot.py
@@ -5,6 +5,7 @@ import logging
 import time
 import collections
 import thread
+import re 
 
 
 from flask import Flask, render_template, request, redirect, url_for, escape, session, make_response, request, jsonify  
@@ -15,6 +16,12 @@ from google.cloud import datastore
 #global variables declaration 
 templist = []
 humlist = []
+dewlist = []
+PM1list = []
+PM25list = []
+PM10list = []
+datalist = []
+parseddata = []
 
 #logging configuration
 #@todo - need to move it to a configuration file
@@ -88,35 +95,89 @@ def get_gdstore_data():
     print "enter into gdstore function"
     client = datastore.Client()
     logger.info(client)
-    query = client.query(kind='senval')
-    query.add_filter('Temp', '>', 2.00)
-    #query.add_filter('Humidity', '>', 10.00)
-    result = list(query.fetch())
+    query = client.query(kind='Thermo')
+    query.add_filter('device_id', '=', '1c002e000347363339343638')
+    result = list(query.fetch(limit = 1000))
     logger.info("Data fetched from server is")
-    print result
-    #logger.info(result)
-    prepare_templist(result)
-    prepare_humiditylist(result)
-
-
-def prepare_templist(result):
-    for data in result:
+    prepare_data(result)
+    
+def prepare_data(result):
+    for data in result: 
         try:
-            print "Temperature : {0}".format(data['Temp'])
-            templist.append(data['Temp'])
-            #print templist 
+            print "data is : {0} and published time is {1}".format(data['data'], data['published_at'])
+            time = data['published_at']
+            datalist.append(data['data'])
+            parseddata = re.split(',', data['data'])
+            prepare_humiditylist(parseddata, time) 
+            prepare_templist(parseddata, time)
+            prepare_dewlist(parseddata, time)
+            prepare_pm1list(parseddata, time)
+            prepare_pm25list(parseddata, time)
+            prepare_pm10list(parseddata, time)
+            
         except KeyError:
-           pass 
+            pass
 
-def prepare_humiditylist(result):
-    for data in result:
-        try:
-            print "Humidity : {0}".format(data['Humidity '])
-            humlist.append(data['Humidity '])
-            #print humlist
-        except KeyError:
-           pass
 
+def prepare_humiditylist(parseddata,time ):
+    Humidity = parseddata[0]
+    Humidity = re.split('=', Humidity)
+    Humidity = Humidity[1]
+    humiditydict = {}
+    humiditydict.update({"time":time})
+    humiditydict.update({"humidity": Humidity})
+    humlist.append(humiditydict)
+    print "Humidity List is {0} \r\n ".format( humlist )
+
+def prepare_templist(parseddata,time):
+    Temperature = parseddata[1]
+    Temperature = re.split('=', Temperature)
+    Temperature = Temperature[1]
+    tempdict = {}
+    tempdict.update({"time": time})
+    tempdict.update({"temperature": Temperature})
+    templist.append(tempdict)
+    print "Temperature list is {0}".format( templist )
+
+def prepare_dewlist(parseddata,time):
+    Dew = parseddata[2]
+    Dew = re.split('=', Dew)
+    Dew = Dew[1]
+    dewdict = {}
+    dewdict.update({"time" :time})
+    dewdict.update({"dew" : Dew})
+    dewlist.append(dewdict)
+    print "Dew List is {0}".format( dewlist )
+
+def prepare_pm1list(parseddata, time):
+    PM1 = parseddata[3]
+    PM1 = re.split('=', PM1)
+    PM1 = PM1[1]
+    pm1dict = {}
+    pm1dict.update({"time" : time})
+    pm1dict.update({"pm1" : PM1})
+    PM1list.append(pm1dict)
+    print "PM1 list is {0}".format(PM1list)
+
+def prepare_pm25list(parseddata, time):
+    PM25 = parseddata[4]
+    PM25 = re.split('=', PM25)
+    PM25 = PM25[1]
+    pm25dict = {}
+    pm25dict.update({"time" : time})
+    pm25dict.update({"pm25" : PM25})
+    PM25list.append(pm25dict)
+    print "PM25 list is {0}".format(PM25list)
+
+def prepare_pm10list(parseddata, time):
+    PM10 = parseddata[5]
+    PM10 = re.split('=', PM10)
+    PM10 = PM10[1]
+    pm10dict = {}
+    pm10dict.update({"time" : time})
+    pm10dict.update({"pm10" : PM10})
+    PM10list.append(pm10dict)
+    print "PM10 list is {0}".format(PM10list)            
 
 
 
